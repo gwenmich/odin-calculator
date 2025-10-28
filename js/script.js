6ddffb2097d33bca4e0db86979a1b89c88021eef
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2025-10-28 20:50:25 +0000

    Build all buttons with respective classes

--- js/script.js
+++ js/script.js
@@ -18,21 +18,65 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
   
   switch (operator) {
-    case '+':
+    case "+":
       return add(num1, num2);
       break;
-    case '-':
+    case "-":
       return subtract(num1, num2);
       break;
-    case '*':
+    case "*":
       return multiply(num1, num2);
       break;
-    case '/':
+    case "/":
       return divide(num1, num2);
       break;
   }
 }
 
 const display = document.querySelector("#display");
+const numsDiv = document.querySelector(".number-btns");
+const opersDiv = document.querySelector(".operator-btns");
 
-console.log(operate('+', 1, 2));
+
+function buildNumButns() {
+
+  for (let i = 0; i < 10; i++) {
+      const numBtn = document.createElement("button");
+      numBtn.classList.add("num");
+      numBtn.textContent = i;
+      numsDiv.appendChild(numBtn);
+  }
+
+  const dotBtn = document.createElement("button");
+  dotBtn.classList.add("dot");
+  dotBtn.textContent = ".";
+  numsDiv.appendChild(dotBtn);
+}
+
+
+
+function buildOperatorBtns() {
+  const operators = [
+    {"+": "plus"},
+    {"-": "minus"},
+    {"*": "times"},
+    {"/": "divide"}
+  ]
+
+  for (const operator of operators) {
+    const operBtn = document.createElement("button");
+    const [[symbol, btnClass]] = Object.entries(operator);
+    
+    operBtn.textContent = symbol;
+    operBtn.classList.add(btnClass);
+    
+    opersDiv.appendChild(operBtn);
+  }
+}
+
+
+
+buildNumButns();
+buildOperatorBtns();
+
+console.log(operate("+", 1, 2));
