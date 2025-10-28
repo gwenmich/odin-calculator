f40d5f7bbf21a4879b7fa88c216cde6b5a5181bd
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2025-10-28 16:51:55 +0000

    Create base logic and display

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function add(num1, num2) {
+  display.textContent = `${num1} + ${num2}`
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+      return multiply(num1, num2);
+      break;
+    case '/':
+      return divide(num1, num2);
+      break;
+  }
+}
+
+const display = document.querySelector("#display");
+
+console.log(operate('+', 1, 2));
